a2952f92fd2f3d3d875929d43f35321a89a3e65e
Munchdoop <44480498+Munchdoop@users.noreply.github.com>
2020-10-29 16:54:39 +0100

    g

--- wargame-game.fs
+++ wargame-game.fs
@@ -18,7 +18,9 @@ let getCard (play : player ) : (card * player) option =
     | x :: xs -> Some(x,xs)
 
 let a = getCard(player1)
-printfn "%A" a
+let c = fst a.Value
+//printfn "%A" a 
+//printfn "%A" c
 
 let addCards (plays : player) (decks : deck) : player =
     let shuffle1 = Wargame_shuffle.shuffle(plays)
@@ -28,6 +30,39 @@ let addCards (plays : player) (decks : deck) : player =
 let b = addCards [1;2] [3;4;5]
 printfn "%A" b
 
+
+let rec game (board:card list) (player1:player) (player2:player) : int =
+    let a = getCard(player1)
+    let b = getCard(player2)
+    //printfn "%A %A" a b //test  
+    if fst a.Value > fst b.Value then 1 
+    elif fst a.Value < fst b.Value then 2 
+    elif fst a.Value = fst b.Value then
+        let c = fst a.Value :: board 
+        let d = fst b.Value :: c
+        let cardFaceDown1 = getCard(player1)
+        let cardFaceDown2 = getCard (player2)
+        let facedownappend1 = fst cardFaceDown1.Value :: d 
+        let facedownappend2 = fst cardFaceDown2.Value :: facedownappend1
+        let newWar1 = getCard(player1)
+        let newWar2 = getCard(player2)
+        if fst newWar1.Value > fst newWar2.Value then 4 
+        elif fst newWar1.Value < fst newWar2.Value then 5
+        else 3       
+    else 0 //krigsfunktion
+
+(*let krig (play1: player) (play2:player) =
+    let board = []
+    let a = getCard(play1)
+    let b = getCard(play2)
+    let c = fst a.Value :: board 
+    let d = fst b.Value :: c
+    d
+*)
+//let g = krig player1 player2 
+//printf "%A" g
+let f = game [] player1 player2
+printfn "%A" f
 
 
 (*skal være tre outcome, a > b, a = b, a < b
