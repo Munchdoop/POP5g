fdcfea38fb59cfe206ea56009a5b1c9b038c3477
Munchdoop <44480498+Munchdoop@users.noreply.github.com>
2020-10-29 14:39:26 +0100

    shuffle er done

---
+++ wargame-game.fs
@@ -0,0 +1,9 @@
+// open wargame 
+//implemter module ordentligt
+
+type player = deck 
+
+
+
+(*skal være tre outcome, a > b, a = b, a < b*)
+
--- wargame-shuffle.fs
+++ wargame-shuffle.fs
@@ -1,3 +1,5 @@
+// module wargame 
+//sørg for libary virker 
 type card = int
 type deck = card list 
 
@@ -13,48 +15,21 @@ let deal (d: deck) : deck*deck =
 let a = deal([4;9;2;5;3])
 let b = deal([])
 let c = deal([2])
-printf "%A" a
-printf "%A" b
-printf "%A" c
 
-//randomfunktion - makes random list 
-let randlist (n:int) =
-    let rnd = System.Random()
-    List.init n (fun _ -> rnd.Next (2,15))
-
-let deck1 = randlist 10
-printfn "%A" deck1
-
-//shuffle funktion, uses perfect shuffle (see faro shuffle)
+let rand : int -> int = let rnd = System.Random ()
+                        in fun n -> rnd.Next (0,n)
+//shuffle funktion
 let shuffle (de:deck) : deck =
-    let divide = (List.length de) / 2
-    let a = List.mapi (fun x y -> (if x < divide then x * 2 else ((x-divide)*2)+1), y) de
+    let a = List.mapi (fun x y -> rand(52), y) de
     let b = List.sortBy fst a
     List.map snd b
-
-let f = shuffle(deck1)
-
-let rand : int -> int = let rnd = System . Random ()
-                        in fun n -> rnd . Next (0,n)
 
 
 //newdeck - skal lave et dæk af 4 serier med 2-14 tal (et reelt kortdæk)
 let dec = [2;3;4;5;6;7;8;9;10;11;12;13;14]
 let newdeck() :deck = 
-    let dec2 = shuffle(dec)
-    let dec3 = List.append dec dec  
-    let sh = shuffle(dec3)
-    let sh1 = List.append sh sh 
-    let sh2 = shuffle(sh1)
-    sh2 
+    let deckappend1 = dec @ dec @ dec @ dec
+    let shuffle3 = shuffle(deckappend1)
+    shuffle3 
 
 newdeck()
-(*
-let a = newdeck()
-let newdeck() : deck =
-    let a = []:int list
-    a :: rand(15)  
-    a 
-
-newdeck()
-*)
