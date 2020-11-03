cc0c29470948dbac6b2725fdae465ed334ee9e00
Jeuwls <71440860+Jeuwls@users.noreply.github.com>
2020-11-03 20:25:40 +0100

    wargame-game-pm test

--- wargame-game-pm.fs
+++ wargame-game-pm.fs
@@ -38,5 +38,29 @@ let rec game (board:deck) (player1:player) (player2:player) (acc:int) : int*int 
                     game cardPile p1Deck p2Deck (acc+1)
                 | _ -> -1, acc
 
-let f = game [] player1 player2 0
-printfn "%A" f
+//test and stats
+///<summary> counts wins, ties and average game length. </summary>
+///<param name = "n"> an int </param>
+///<returns> returns a string containing wins, ties and average game length. </param>
+let test (n:int) =
+    let mutable winsPlayer1 = 0 
+    let mutable winsPlayer2 = 0
+    let mutable ties = 0
+    let mutable accumulatedPlays = 0 
+    for i = 1 to n do 
+        let res = game [] player1 player2 0
+        accumulatedPlays <- snd res + accumulatedPlays
+        match fst res with
+            | r when r = 1 ->
+                winsPlayer1 <- winsPlayer1 + 1  
+                winsPlayer1
+            | r when r = 2 ->
+                winsPlayer2 <- winsPlayer2 + 1
+                winsPlayer2
+            | r when r = 0 ->
+                ties <- ties + 1 
+                ties
+            | _ -> 5
+    let average = accumulatedPlays / n 
+    printfn "Wins player1: %A, wins player2: %A, ties: %A, average: %A" winsPlayer1 winsPlayer2 ties average
+printfn "%A" (test 10000)
