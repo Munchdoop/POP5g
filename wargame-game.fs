0a811df5536ceca1ff863c20e90ceec29fe1c9ff
Munchdoop <44480498+Munchdoop@users.noreply.github.com>
2020-11-03 12:29:45 +0100

    code done

--- wargame-game.fs
+++ wargame-game.fs
@@ -1,95 +1,81 @@
+///<summary =""></summary>
 type card = int
 type deck = card list
 type player = deck
 
 let deckplay = Wargame_shuffle.newdeck()
-
 let players = Wargame_shuffle.deal(deckplay)
-
 let player1 = fst players
-
 let player2 = snd players
 
+//getCard function 
+///<summary>Takes a player type and returns a tuple containing the head and the tail of players as an option.</summary>
+///<param name="play">A type player, see also player.</param>
+///<returns>A tuple containing the head and tail of <paramref name="player"/> as an option.</returns>
 let getCard (play : player ) : (card * player) option =
     match play with
     | [] -> None
     | x :: xs -> Some(x,xs)
 
+//addCards function
+///<summary>This function takes a player type and returns a list of integers</summary>
+///<param name = "board"> a player type </param>
+///<param name = "decks"> a deck type </param>
+///<returns> a deck with board appended and shuffled </returns>
 let addCards (board : player) (decks : deck) : player =
     let shuffle1 = Wargame_shuffle.shuffle(board)
     let a = decks @ shuffle1
     a
 
-let rec game (board:card list) (player1:player) (player2:player) (count : int) : int =
+//game function
+///<summary> a recursive function that plays a game of the card game war </summary>
+///<param name="board"> a int list </param>
+///<param name="player1"> a player which is a int list </param>
+///<param name="player2"> a player which is a int list </param>
+///<returns> returns the result of the war game represented as either 0, 1 or 2 </returns>
+let rec game (board:card list) (player1:player) (player2:player) : int =
     let draw1 = getCard(player1)
     let draw2 = getCard(player2)
-    if fst draw1.Value > fst draw2.Value then //tjekker om player1 har vundet runden
-        let boardAppend = fst draw1.Value :: fst draw2.Value :: board //bordet der kan vindes
+    if fst draw1.Value > fst draw2.Value then //checks is player1 wins round
+        let boardAppend = fst draw1.Value :: fst draw2.Value :: board
         let player1Update = snd draw1.Value
         let player1Next = addCards boardAppend player1Update
         let player2Next = snd draw2.Value
-        let count0 = count + 1 
         if List.isEmpty player2Next then 
-            printfn "%d" count0 
             1 
-        //tjekker om player2 har vundet spillet
-        else game [] player1Next player2Next count0 //hvis ingen vinder spillet så ny runde
+        //checks if player1 won game
+        else game [] player1Next player2Next  //if no winner 
     elif fst draw1.Value < fst draw2.Value then //player two wins round
         let boardAppend = fst draw1.Value :: fst draw2.Value :: board
         let player2Update = snd draw2.Value
         let player1Next = snd draw1.Value
         let player2Next = addCards boardAppend player2Update
-        let count0 = count + 1 
         if List.isEmpty player1Next then 
-            printfn "%d" count0 
             2
-        else game [] player1Next player2Next count0
-    elif fst draw1.Value = fst draw2.Value then //war - but doesnt work
+        else game [] player1Next player2Next
+    elif fst draw1.Value = fst draw2.Value then //war
         let boardUpdate = fst draw1.Value :: fst draw2.Value :: board
         let player1Update = snd draw1.Value
         let player2Update = snd draw2.Value
-        let count0 = count + 1 
-        if getCard(player1Update) = None then 
-            printfn "%d" count0 
+        if getCard player1Update = None then 
             2
-        elif getCard(player2Update) = None then 
-            printfn "%d" count0 
+        elif getCard player2Update = None then 
             1
         else
-            let cardFaceDown1 = getCard(player1Update) //kortet som ikke tælles med
-            let cardFaceDown2 = getCard(player2Update) //kortet som ikke tælles med billedside ned
+            let cardFaceDown1 = getCard(player1Update) 
+            let cardFaceDown2 = getCard(player2Update)
             let boardUpdate0 = fst cardFaceDown1.Value :: fst cardFaceDown2.Value :: boardUpdate
             let player1Update1 = snd cardFaceDown1.Value
             let player2Update1 = snd cardFaceDown2.Value
             if List.isEmpty player1Update1 then
-                printfn "%d" count0 
                 2
             elif List.isEmpty player2Update1 then
-                printfn "%d" count0 
                 1
             elif List.isEmpty player2Update1 && List.isEmpty player1Update1 then
-                printfn "%d" count0 
                 0
             else
-                game boardUpdate0 player1Update1 player2Update1 count0
+                game boardUpdate0 player1Update1 player2Update1 
     else 0
-         //gameend tie not implemented
 
-(*let krig (play1: player) (play2:player) =
-    let board = []
-    let a = getCard(play1)
-    let b = getCard(play2)
-    let c = fst a.Value :: board
-    let d = fst b.Value :: c
-    d
-*)
-//let g = krig player1 player2
-//printf "%A" g
-let f = game [] player1 player2 0
+let f = game [] player1 player2
 printfn "%A" f
-
-
-(*skal være tre outcome, a > b, a = b, a < b
-skal også have en stop funktion som slutter når en player rammer 0
-sørg for både at implementere vindere og ties *)
-
--- wargame-shuffle.fs
+++ wargame-shuffle.fs
@@ -1,10 +1,11 @@
 module Wargame_shuffle
-
-//sørg for libary virker 
 type card = int
 type deck = card list 
 
-//deal-funktion
+//deal-function
+///<summary> takes a deck and divides it into two new decks one int at a time.</summary>
+///<param name="d"> a deck type, which is a card list. </param> 
+/// <returns> The function returns a tuple which contains two stacks. </returns>
 let deal (d: deck) : deck*deck =
     let rec stack dl (xs, ys) : deck*deck =
         match dl with
@@ -13,24 +14,23 @@ let deal (d: deck) : deck*deck =
         | _ -> (xs, ys)
     stack d ([], [])
 
-let a = deal([4;9;2;5;3])
-let b = deal([])
-let c = deal([2])
-
 let rand : int -> int = let rnd = System.Random()
                         in fun n -> rnd.Next (0,n)
-//shuffle funktion
+
+//shuffle function
+///<summary> shuffles the deck by assigning random indices and sorting them. </summary>
+///<param name="de"> a deck, which is a card list. </param>
+///<returns> Returns the now shuffles deck, without the assigned indices. </returns>
 let shuffle (de:deck) : deck =
     let a = List.mapi (fun i y -> rand(52), y) de
     let b = List.sortBy fst a
     List.map snd b
 
-
-//newdeck - skal lave et dæk af 4 serier med 2-14 tal (et reelt kortdæk)
+//newdeck()
+///<summary> a unit type that makes a random shuffled card deck containing 52 cards. </summary>
+///<returns> returns a shuffled card deck. </returns>
 let newdeck() :deck = 
     let dec = [2;3;4;5;6;7;8;9;10;11;12;13;14]
     let deckappend1 = dec @ dec @ dec @ dec
     let shuffle3 = shuffle(deckappend1)
     shuffle3 
-
-newdeck()
