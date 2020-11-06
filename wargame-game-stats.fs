9289a5e7be00baf2dc37f90b8f1848a61c11a161
Jeuwls <71440860+Jeuwls@users.noreply.github.com>
2020-11-06 15:45:52 +0100

    wargame-game-pm delete
    wargame-game-pm deleted, wargame-game and wargame-game-stats updated

--- wargame-game-stats.fs
+++ wargame-game-stats.fs
@@ -41,11 +41,19 @@ let rec game (board:deck) (player1:player) (player2:player) (acc:int) : int*int 
             let cardPile : deck = p1Card :: p2Card :: board
             match p1Card, p2Card with
                 | p1Card, p2Card when p1Card > p2Card -> 
-                    game [] (addCards p1Deck cardPile) p2Deck (acc+1)
+                    game [] (addCards cardPile p1Deck) p2Deck (acc+1)
                 | p1Card, p2Card when p1Card < p2Card ->
-                    game [] p1Deck (addCards p2Deck cardPile) (acc+1)
+                    game [] p1Deck (addCards cardPile p2Deck) (acc+1)
                 | p1Card, p2Card when p1Card = p2Card ->
-                    game cardPile p1Deck p2Deck (acc+1)
+                    match getCard p1Deck, getCard p2Deck with 
+                        | None, None ->
+                            game (cardPile) [] [] (acc+1)
+                        | Some (p1CardWar, p1CardDeckWar), None ->
+                            game (p1CardWar :: cardPile) p1CardDeckWar [] (acc+1)
+                        | None, Some (p2CardWar, p2CardDeckWar) ->
+                            game (p2CardWar :: p2CardWar :: cardPile) [] p2CardDeckWar (acc+1)
+                        | Some (p1CardWar, p1CardDeckWar), Some (p2CardWar, p2CardDeckWar) ->
+                            game (p1CardWar:: p2CardWar :: cardPile) p1CardDeckWar p2CardDeckWar (acc+1)
                 | _ -> -1, acc
 
 //test and stats
