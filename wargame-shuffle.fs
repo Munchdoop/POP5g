4bb8701b24379d53ee73b8a5ae75e2ac6eb32361
Jeuwls <71440860+Jeuwls@users.noreply.github.com>
2020-11-07 15:35:40 +0100

    Update wargame-shuffle.fs
    In-code documentation revision

--- wargame-shuffle.fs
+++ wargame-shuffle.fs
@@ -5,9 +5,10 @@ type deck = card list
 
 module Shuffle =
     //deal-function
-    ///<summary> takes a deck and divides it into two new decks one int at a time.</summary>
-    ///<param name="d"> a deck type, which is a card list. </param> 
-    /// <returns> The function returns a tuple which contains two stacks. </returns>
+    /// <summary>Takes a <typeparamref name="deck"/> and divides it into two new lists,
+    /// alternating between one <typeparamref name="card"/> at a time per <typeparamref name="deck"/>.</summary>
+    /// <param name="d"> a <typeparamref name="deck"/>, which is a <typeparamref name="card list"/>.</param> 
+    /// <returns>A tuple which contains two separate <typeparamref name="deck"/> instances.</returns>
     let deal (d: deck) : deck*deck =
         let rec stack dl (xs, ys) : deck*deck =
             match dl with
@@ -15,22 +16,26 @@ module Shuffle =
             | [x] -> (x::xs, ys)
             | _ -> (xs, ys)
         stack d ([], [])
-
+    
+    //random function
+    /// <summary>Generates a random integer between 0 and <paramref name="n"/>.</summary>
+    /// <param name="n"> an <c>int</c>.</param>
+    /// <returns>A random integer between 0 and <paramref name="n"/>.</returns>
     let rand : int -> int = let rnd = System.Random()
                             in fun n -> rnd.Next (0,n)
 
     //shuffle function
-    ///<summary> shuffles the deck by assigning random indices and sorting them. </summary>
-    ///<param name="de"> a deck, which is a card list. </param>
-    ///<returns> Returns the now shuffles deck, without the assigned indices. </returns>
+    /// <summary>Shuffles an input <typeparamref name="deck"/> by assigning random indices and sorting through them.</summary>
+    /// <param name="de"> a <typeparamref name="deck"/>, which is a <typeparamref name="card list"/>.</param> 
+    /// <returns> Returns the now shuffled <paramref name="de"/> (without the assigned indices). </returns>
     let shuffle (de:deck) : deck =
         let a = List.mapi (fun i y -> rand(52), y) de
         let b = List.sortBy fst a
         List.map snd b
 
     //newdeck()
-    ///<summary> a unit type that makes a random shuffled card deck containing 52 cards. </summary>
-    ///<returns> returns a shuffled card deck. </returns>
+    /// <summary>Creates a random shuffled <typeparamref name="deck"/> containing 52 cards.</summary>
+    /// <returns>A shuffled <typeparamref name="deck"/>.</returns>
     let newdeck() :deck = 
         let dec = [2;3;4;5;6;7;8;9;10;11;12;13;14]
         let deckappend1 = dec @ dec @ dec @ dec
