26dd4637c1d6024acff93c9c16744ef0ac8bad24
Jeuwls <71440860+Jeuwls@users.noreply.github.com>
2020-12-18 19:02:41 +0100

    roguelike work

--- roguelike-game.fsx
+++ roguelike-game.fsx
@@ -1,15 +1,10 @@
-namespace Roguelike
-//open RoguelikeLib
-//#r "roguelike-game.dll"
+open Roguelike
+#r "roguelike-game.dll"
 
-module Game =
-
-    let game = Library.World(20,20)
-
-    //let myList = game.CreateRoom(10,10)
-    //for elm in myList do game.AddItem(elm)
-    game.Play()
-
-    printfn "Press any key to reset console..."
-    let x = System.Console.ReadKey()
-    System.Console.ResetColor()
+let game = Roguelike.World(20,20)
+while not (game.GetExit()) do
+    game.Play() 
+    printfn "Enter a command"
+printfn "Press any key to reset console..."
+let x = System.Console.ReadKey()
+System.Console.ResetColor()
