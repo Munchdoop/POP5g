4acf68f92dccc590c28135d8a3fd2d1e10d3734c
Jeuwls <71440860+Jeuwls@users.noreply.github.com>
2020-12-21 22:44:55 +0100

    r

---
+++ experiment.fsx
@@ -0,0 +1,4 @@
+let arrayOfArrays = [| [| 1.0; 0.0 |]; [|0.0; 1.0 |] |]
+let twoDimensionalArray = Array2D.init 2 2 (fun i j -> arrayOfArrays.[i].[j])
+printfn "%A" arrayOfArrays
+printfn "%A" twoDimensionalArray
