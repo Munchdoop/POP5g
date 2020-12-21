85c592a03a579a5fd1622e882e07488da464f9d7
Jeuwls <71440860+Jeuwls@users.noreply.github.com>
2020-12-21 21:57:34 +0100

    r

--- roguelike.fs
+++ roguelike.fs
@@ -1,13 +1,17 @@
 module Roguelike
 
 type Color = System.ConsoleColor
+type Tile = char*Color*Color
 
-type Canvas(dims:int*int) =
+type Canvas(dims:int*int) = // Canvas renders the map
     let (rows, cols) = dims
     let field = Array2D.create rows cols ('.', Color.White, Color.Black)
     member this.Set(cx:int, cy:int, cc:char, cfg:Color, cbg:Color) =
         field.[cx,cy] <- (cc,cfg,cbg)
-    member this.Show () = 
+    member this.SetOnOccupied(cx:int, cy:int, cc:char, cfg:Color) =
+        let (_,_,obg) = field.[cx,cy]
+        in this.Set(cx,cy,cc,cfg,obg)
+    member this.Show () =
         System.Console.Clear()
         System.Console.ResetColor()
         for i = 0 to rows-1 do
@@ -17,6 +21,7 @@ type Canvas(dims:int*int) =
                 System.Console.BackgroundColor <- bg
                 printf "%c" c
             printf "\n"
+     member this.Coord (x:int,y:int) = field.[x,y]
 
 [<AbstractClass>]
 type Entity(xInit:int, yInit:int, cInit:char, fgInit:Color, bgInit:Color) =
@@ -28,7 +33,7 @@ type Entity(xInit:int, yInit:int, cInit:char, fgInit:Color, bgInit:Color) =
     member val fg = fgInit with get
     member val bg = bgInit with get
     abstract member RenderOn : Canvas -> unit
-    default this.RenderOn (canvas:Canvas) = 
+    default this.RenderOn (canvas:Canvas) =
         canvas.Set(this.x,this.y,this.c,this.fg,this.bg)
 
 type Player(xInit, yInit) =
@@ -38,46 +43,75 @@ type Player(xInit, yInit) =
     member this.IsDead = hp <= 0
     member this.Damage(dmg:int) = hp <- (hp - dmg)
     member this.Heal(h:int) = hp <- (hp + h)
-    member this.MoveTo(dx:int,dy:int) =
-        this.x <- this.x + dx
-        this.y <- this.y + dy
+    member this.MoveTo(dx:int,dy:int) = //direction-x, direction-y
+        //if
+        //this.RenderOn (this.x,this.y,".",Color.White, Color.Black)
+        this.x <- this.x + dx // -1 to move left, 1 to move right
+        this.y <- this.y + dy // -1 to move up, 1 to move down
+    override this.RenderOn (canvas:Canvas) =
+        canvas.SetOnOccupied(this.x,this.y,this.c,this.fg)
+
 
 [<AbstractClass>]
 type Item(xInit, yInit, cInit, fgInit, bgInit) =
     inherit Entity(xInit, yInit, cInit, fgInit, bgInit)
     abstract member InteractWith : Player -> unit
+
     member this.FullyOccupy : bool = false
 
 type Water(xInit, yInit) =
-    inherit Item(xInit, yInit, ' ', Color.White, Color.Blue)
+    inherit Item(xInit, yInit, '~', Color.White, Color.Blue)
     override this.InteractWith(player:Player) = player.Heal(2)
     member this.FullyOccupy = false
 
-type Wall(xInit, yInit) = 
-    inherit Item(xInit, yInit, ' ',Color.White,Color.White)
+type Wall(xInit, yInit) =
+    inherit Item(xInit, yInit, '|',Color.White,Color.White)
     override this.InteractWith(_) = ()
     member this.FullyOccupy = true
 
 type Fire (xInit, yInit)=
-    inherit Item(xInit, yInit, ' ',Color.White,Color.Red)
+    inherit Item(xInit, yInit, '^',Color.White,Color.Red)
     let mutable fireLeft = 5
     override this.InteractWith (p:Player) =
-        p.Damage 2
+        p.Damage(1)
         fireLeft <- fireLeft - 1
     member this.FullyOccupy = false
+    //fire needs to be deleted after 5 touches
 
-type FleshEatingPlant (xInit, yInit) = 
+type FleshEatingPlant (xInit, yInit) =
     inherit Item (xInit, yInit, 'F', Color.White, Color.Green)
-    override this.InteractWith (p:Player) = p.Damage 5
+    override this.InteractWith (p:Player) = p.Damage(5)
+    member this.FullyOccupy = true
+(*
+type Monster(xInit,yInit) =
+    inherit Item(xInit,yInit,'Ø', Color.Green, Color.Black)
+    let mutable hp = 5
+    member this.HP = hp
+    member this.Killed = hp <= 0
+    //if killed then removed
+    override this.InteractWith (p:Player) =
+        p.Damage(2)
+        this.Damage(2) //monster takes damage also
     member this.FullyOccupy = false
+*)
+//'D', Color.White, Color.Green)
 
-type Pot (xInit, yInit)= 
+    //member this.Open =
+        //override this.InteractWith (p:Player) =
+        //if p.HitPoints >= 5 then
+            //true
+        //else
+            //false
+    //change to true if player has 5 health
+//    member this.FullyOccupy = true
+
+type Pot (xInit, yInit)=
     inherit Item (xInit, yInit, 'U', Color.White, Color.DarkMagenta)
-    let mutable potHealth = 1 
+    let mutable potHealth = 1
     override this.InteractWith (p:Player) =
         p.Heal(2)
         potHealth <- potHealth - 1
-        //pot needs to be deleted after one use 
+        //pot needs to be deleted after one use
 
 
 
@@ -85,22 +119,26 @@ type Pot (xInit, yInit)=
 let io (p:Player) =
     //printfn "Enter a command: "
     match System.Console.ReadKey().Key with
-    | System.ConsoleKey.UpArrow -> p.MoveTo(-1,0)
+    | System.ConsoleKey.UpArrow ->
+        //printfn "Moving player up"
+        p.MoveTo(-1,0)
     | System.ConsoleKey.DownArrow -> p.MoveTo(1,0)
     | System.ConsoleKey.LeftArrow -> p.MoveTo(0,-1)
     | System.ConsoleKey.RightArrow -> p.MoveTo(0,1)
     | System.ConsoleKey.Q -> p.Damage(10)
     | _ -> p.MoveTo(0,0)
 
-type World(w:int, h:int) =
-    let mutable level = Canvas(w,h)
+type World(h:int, w:int) =
+    let mutable level = Canvas(h,w)
+    let mutable itemList = Array2D.init h w (fun y x -> Wall(x,y))
     let mutable exit:bool = false
     let player = Player(2,2)
     member this.Exit = exit
     member this.GetExit() = this.Exit
     member this.AddItem (item:Item) =
         level.Set(item.x,item.y,item.c,item.fg,item.bg)
-    (*member this.CreateRoom(w:int, h:int) =
+        item.RenderOn(level)
+    member this.CreateRoom(w:int, h:int) = //add startPost(x,y)
         let mutable wallList = []
         for i=0 to h do
             wallList <- Wall(i,0) :: wallList
@@ -108,12 +146,12 @@ type World(w:int, h:int) =
         for j=0 to w do
             wallList <- Wall(0, j) :: wallList
             wallList <- Wall(w, j) :: wallList
-        wallList*)
-    member this.Play() = 
-        level.Show()
-        player.RenderOn(level)
+        wallList
+    member this.Play() =
+        let myRoom = this.CreateRoom(5,5)
+        for elm in myRoom do
+            this.AddItem (elm)
         while not player.IsDead do
-            io player
-        printfn "Press any key to reset console..."
+            player.RenderOn(level)
         let x = System.Console.ReadKey()
         System.Console.ResetColor()
